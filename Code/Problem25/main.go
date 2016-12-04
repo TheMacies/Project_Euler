427c902a0df6922e160e6bba559deef238e23197
Maciej Długoszek <muloni@gmail.com>
2016-12-04 15:39:18 +0100

    Problem25

---
+++ Code/Problem25/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	a := new(big.Int).SetInt64(1)
+	b := new(big.Int).SetInt64(1)
+	i := 2
+	for len(b.String()) < 1000 {
+		c := b
+		b = new(big.Int).Add(a, b)
+		a = c
+		i++
+	}
+	fmt.Println(i)
+}
