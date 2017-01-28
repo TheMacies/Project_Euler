6075d68319d10eb54ae93031a0a6c8de22ce6ead
Maciej Długoszek <muloni@gmail.com>
2017-01-28 11:49:58 +0100

    Problem577

---
+++ Code/Problems551-600/Problem577/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func countHexagons(l int) int {
+	sum := 0
+	for j := 3; j <= l; j += 3 {
+		sum += j * (1 + l - j + 1) * (l - j + 1) / 6
+	}
+	return sum
+}
+
+func main() {
+	t := time.Now()
+	sum := 0
+	for i := 3; i <= 12345; i++ {
+		sum += countHexagons(i)
+	}
+	fmt.Println(time.Since(t))
+	fmt.Println(sum)
+}
